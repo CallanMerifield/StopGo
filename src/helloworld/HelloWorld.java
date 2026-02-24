510937f069971cb5d068eadb65416f8bee8ecb14
CallanMerifield <st10504936@myemeris.edu.za>
2026-02-24 21:10:20 +0200

    First commit for Hello World/StopSign project

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld;
+
+/**
+ *
+ * @author callan_merifield_st10504936
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("""
+                              *******************                                 *****************
+                             *********************                              *********************
+                            ***********************                            ***********************
+                           *************************                          *************************
+                           *************************                          *************************
+                           **     S  T  O  P      **                          **        G   O        **
+                           *************************                          *************************
+                           *************************                           *************************
+                            ***********************                            ***********************
+                             *********************                              *********************
+                              *******************                                ******************
+                               *****************                                   **************
+                                      | |                                               | |
+                                      | |                                               | |
+                                      | |                                               | |
+                                      | |                                               | |
+                                    _______                                            ______
+                           
+                           
+                           
+                           """);
+        
+    }
+}
